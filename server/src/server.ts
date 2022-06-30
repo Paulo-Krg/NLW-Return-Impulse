0a193703a7fc4b4c2e99f2ba1b917d830b2b3ed2
Paulo Kruger <paulo.krg.costa@gmail.com>
2022-06-29 23:02:31 -0300

    Access-Control-Allow-Origin cors header

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,42 @@
 import express from "express";
 import cors from "cors";
 import { routes } from "./routes";
+import { nextTick } from "process";
 
 const frontEndUrl = process.env.FRONT_END_URL;
 const port = process.env.PORT;
 
 const app = express();
-app.use(
-	cors({
-		origin: `${frontEndUrl}`,
-	})
-);
 app.use(express.json());
+app.use(
+	// cors({
+	// 	origin: process.env.FRONT_END_URL,
+	// })
+	function (req, res, next) {
+		res.setHeader("Access-Control-Allow-Credentials", "true");
+		res.setHeader("Access-Control-Allow-Origin", `${frontEndUrl}`);
+		// res.setHeader("Access-Control-Allow-Origin", "*");
+		// another common pattern
+		// res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+		res.setHeader(
+			"Access-Control-Allow-Methods",
+			"GET,OPTIONS,PATCH,DELETE,POST,PUT"
+		);
+		res.setHeader(
+			"Access-Control-Allow-Headers",
+			"X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+		);
+
+		console.log(req.headers.origin);
+
+		console.log("res: ", res);
+
+		next();
+	}
+	// cors({
+	// 	origin: `${frontEndUrl}`,
+	// })
+);
 app.use(routes);
 
 app.listen(port, () => {
